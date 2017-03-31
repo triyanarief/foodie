a56918ca445a9f794eab249d32bfbdebc6cf0bc0
triyanarief <triyanarief@icloud.com>
2017-03-31 22:51:09 +0700

    session update

--- client/cordova.js
+++ client/cordova.js
@@ -2,8 +2,19 @@ Meteor.startup(function() {
   navigator.geolocation.getCurrentPosition(success);
 });
 
+// success = function(position) {
+//   Session.set('location', position);
+//   debugger
+//   Meteor.call('fetchNearbyLocations', position.coords)
+// }
+
 success = function(position) {
-  Session.set('location', position);
-  debugger
-  Meteor.call('fetchNearbyLocations', position.coords)
+    Session.set('location', {
+        'coords': {
+            'accuracy': position.coords.accuracy,
+            'latitude': position.coords.latitude,
+            'longitude': position.coords.longitude
+        },
+        'timestamp': position.timestamp
+    });
 }
